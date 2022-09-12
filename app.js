9877df2f2563a877ac75fae011ec664153800c2a
Camilo <camilobarrosr@gmail.com>
2022-09-12 17:00:33 -0500

    refactoring and add BEM

--- app.js
+++ app.js
@@ -1,53 +1,59 @@
-import data from "./data.json" assert { type: "json" };
+// import data from "./data.json" assert { type: "json" };
+
+fetch("./data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    const { pokemones_data } = data;
+    const $carrusel_buttons = document.getElementsByClassName("list-buttons");
+
+    $carrusel_buttons[0].addEventListener("click", (event) => {
+      const { id } = event.target;
+      const pk = pokemones_data.find((pokemon) => pokemon.id == id);
+
+      if (pk !== undefined) {
+        const $current_img = document.getElementsByClassName(
+          "card__pokemon-container"
+        )[0];
+        const $current_pokemon = document.getElementsByClassName("table")[0];
+
+        $current_img.innerHTML = `
+          <div class="card__pokemon-name">${pk.name}</div>
+          <div class="card__container-img">
+            <img src=${pk.imgUrl} alt=${pk.name} class="pokemon" />
+          </div>
+        `;
+
+        $current_pokemon.innerHTML = `
+          <div class="table__item">
+            <span class="tomato">NO.</span>
+            <span>${pk.id}</span>
+          </div>
+          <div class="table__item">
+            <span class="tomato">LEVEL</span>
+            <span>${pk.level}</span>
+          </div>
+          <div class="table__item">
+            <span class="tomato">TYPE</span>
+            <span>${pk.type}</span>
+          </div>
+          <div class="table__item">
+            <span class="tomato">HABILITY</span>
+            <span>${pk.hability}</span>
+          </div>
+          <div class="table__item">
+            <span class="tomato">HEIGHT</span>
+            <span>${pk.height}</span>
+          </div>
+          <div class="table__item">
+            <span class="tomato">WEIGHT</span>
+            <span>${pk.weight}</span>
+          </div>
+        </div>
+        `;
+      }
+    });
+  });
 
 /* 
   TODO: Refactorizar el codigo y aplicar delegación de evento. 
 */
-
-
-const { pokemones_data } = data;
-const $carrusel_buttons = document.getElementsByClassName("button_img");
-
-for (let i = 0; i < $carrusel_buttons.length; i++) {
-  $carrusel_buttons[i].addEventListener("click", (event) => {
-    const { id } = event.target;
-    const pk = pokemones_data.find((pokemon) => pokemon.id == id);
-
-    const $current_img = document.getElementsByClassName('pokemon_container')[0]
-    const $current_pokemon =
-      document.getElementsByClassName("pokemon_caract")[0];
-
-    $current_img.innerHTML = `
-      <div class="pokemon_name">${pk.name}</div>
-      <img src=${pk.imgUrl} alt=${pk.name} class="pokemon_img" />
-    `
-
-    $current_pokemon.innerHTML = `
-      <div class="element">
-        <span>NO.</span>
-        <span>${pk.id}</span>
-      </div>
-      <div class="element">
-        <span>LEVEL</span>
-        <span>${pk.level}</span>
-      </div>
-      <div class="element">
-        <span>TYPE</span>
-        <span>${pk.type}</span>
-      </div>
-      <div class="element">
-        <span>HABILITY</span>
-        <span>${pk.hability}</span>
-      </div>
-      <div class="element">
-        <span>HEIGHT</span>
-        <span>${pk.height}</span>
-      </div>
-      <div class="element">
-        <span>WEIGHT</span>
-        <span>${pk.weight}</span>
-      </div>
-    </div>
-    `;
-  });
-}
